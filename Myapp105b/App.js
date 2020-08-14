9d8d6cc6f5edacf4384590f5b8185ea51ede82b1
phakkharaphon <69285817+phakkharaphon@users.noreply.github.com>
2020-08-14 11:29:50 +0700

    add myapp105b

---
+++ Myapp105b/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {view,Text} from 'react-native';
+
+export default function app(){
+  return(
+    <view style={{
+      flex:1,
+      justfyContent:"center",
+      allgnItem:"center",
+    }}>
+      <Text>Hello World</Text>
+    </view>
+  )
+}
